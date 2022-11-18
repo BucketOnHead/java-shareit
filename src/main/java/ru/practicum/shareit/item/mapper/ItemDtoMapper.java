9aa4252dbd95fffd40a3585092714f8d4ab5dc05
IvanMarakanov <marakanovivan@gmail.com>
2022-11-18 19:25:46 +0300

    add: item dto mapper

---
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
@@ -0,0 +1,36 @@
+package ru.practicum.shareit.item.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.repository.UserRepository;
+
+@Component
+@RequiredArgsConstructor
+public class ItemDtoMapper {
+    private final UserRepository userRepository;
+
+    public Item toItem(ItemDto itemDto, Long ownerId) {
+        Item item = new Item();
+
+        item.setId(itemDto.getId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setIsAvailable(itemDto.getAvailable());
+        item.setOwner(userRepository.getUserById(ownerId));
+
+        return item;
+    }
+
+    public ItemDto toItemDto(Item item) {
+        ItemDto itemDto = new ItemDto();
+
+        itemDto.setId(item.getId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.getIsAvailable());
+
+        return itemDto;
+    }
+}
