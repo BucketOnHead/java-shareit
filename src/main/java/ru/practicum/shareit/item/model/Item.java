cd932b3efc5653459bf877b301cfe58768af7641
IvanMarakanov <marakanovivan@gmail.com>
2022-11-18 19:19:35 +0300

    add: class model for things

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.item.model;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+@Data
 public class Item {
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Boolean isAvailable;
+
+    private User owner = new User();
+
+    private ItemRequest request;
 }
