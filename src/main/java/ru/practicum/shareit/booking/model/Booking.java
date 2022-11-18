5afbfa5fb26f6b602ce084787c0a526d59124ad0
IvanMarakanov <marakanovivan@gmail.com>
2022-11-18 19:22:18 +0300

    add: class model for bookings

---
+++ src/main/java/ru/practicum/shareit/booking/model/Booking.java
@@ -0,0 +1,17 @@
+package ru.practicum.shareit.booking.model;
+
+import lombok.Data;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Data
+public class Booking {
+    public enum Status {WAITING, APPROVED, REJECTED, CANCELED}
+
+    private Long id;
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+    private User booker;
+    private Status status;
+}
