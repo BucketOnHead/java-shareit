de8716a0b5d9677dcc5a9ee483e44795e7cc1a67
IvanMarakanov <marakanovivan@gmail.com>
2022-12-08 16:31:07 +0300

    update: user

---
+++ src/main/java/ru/practicum/shareit/user/dto/out/ShortUserDto.java
@@ -0,0 +1,12 @@
+package ru.practicum.shareit.user.dto.out;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+public class ShortUserDto {
+    private Long id;
+}
