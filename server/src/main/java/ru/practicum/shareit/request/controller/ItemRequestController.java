184aaade4f3d2374c2594373235826eeb55d188e
ivanmarakanov <marakanovivan@gmail.com>
2023-03-12 08:32:54 +0300

    style: ItemRequestController refactoring

--- server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,12 +1,11 @@
 package ru.practicum.shareit.request.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.constants.HttpHeadersConstants;
 import ru.practicum.shareit.request.dto.request.RequestItemRequestDto;
 import ru.practicum.shareit.request.dto.response.ItemRequestDto;
 import ru.practicum.shareit.request.service.ItemRequestService;
-import ru.practicum.shareit.validation.group.CreationGroup;
 
 import java.util.List;
 
@@ -18,29 +17,33 @@ public class ItemRequestController {
 
     @PostMapping
     public ItemRequestDto addItemRequest(
-            @Validated(CreationGroup.class) @RequestBody RequestItemRequestDto requestDto,
-            @RequestHeader("X-Sharer-User-Id") Long userId) {
+            @RequestBody RequestItemRequestDto requestDto,
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
+    ) {
         return itemRequestService.addItemRequest(requestDto, userId);
     }
 
     @GetMapping
-    public List<ItemRequestDto> getItemRequestsByRequesterId(
-            @RequestHeader("X-Sharer-User-Id") Long userId) {
+    public Iterable<ItemRequestDto> getItemRequestsByRequesterId(
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
+    ) {
         return itemRequestService.getItemRequestsByRequesterId(userId);
     }
 
     @GetMapping("/{itemRequestId}")
     public ItemRequestDto getItemRequestsById(
-            @RequestHeader("X-Sharer-User-Id") Long userId,
-            @PathVariable Long itemRequestId) {
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId,
+            @PathVariable Long itemRequestId
+    ) {
         return itemRequestService.getItemRequestById(itemRequestId, userId);
     }
 
     @GetMapping("/all")
     public List<ItemRequestDto> getPageWithItemRequestsByRequesterId(
-            @RequestHeader("X-Sharer-User-Id") Long userId,
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId,
             @RequestParam Integer from,
-            @RequestParam Integer size) {
+            @RequestParam Integer size
+    ) {
         return itemRequestService.getItemRequestsByRequesterId(userId, from, size);
     }
 }
