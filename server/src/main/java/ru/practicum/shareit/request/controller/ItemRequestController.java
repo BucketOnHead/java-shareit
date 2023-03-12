356a514e5468949dfdcc6623718a5932323a3c26
ivanmarakanov <marakanovivan@gmail.com>
2023-03-12 09:08:19 +0300

    style: ItemRequestController refactoring

--- server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/controller/ItemRequestController.java
@@ -1,16 +1,19 @@
 package ru.practicum.shareit.request.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.constants.HttpHeadersConstants;
 import ru.practicum.shareit.request.dto.request.RequestItemRequestDto;
 import ru.practicum.shareit.request.dto.response.ItemRequestDto;
+import ru.practicum.shareit.request.logger.ItemRequestControllerLoggerHelper;
 import ru.practicum.shareit.request.service.ItemRequestService;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/requests")
+@Slf4j
 @RequiredArgsConstructor
 public class ItemRequestController {
     private final ItemRequestService itemRequestService;
@@ -18,32 +21,36 @@ public class ItemRequestController {
     @PostMapping
     public ItemRequestDto addItemRequest(
             @RequestBody RequestItemRequestDto requestDto,
-            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long currentUserId
     ) {
-        return itemRequestService.addItemRequest(requestDto, userId);
+        ItemRequestControllerLoggerHelper.addItemRequest(log, requestDto, currentUserId);
+        return itemRequestService.addItemRequest(requestDto, currentUserId);
     }
 
     @GetMapping
     public Iterable<ItemRequestDto> getItemRequestsByRequesterId(
             @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
     ) {
+        ItemRequestControllerLoggerHelper.getItemRequestDtosByRequesterId(log, userId);
         return itemRequestService.getItemRequestsByRequesterId(userId);
     }
 
-    @GetMapping("/{itemRequestId}")
-    public ItemRequestDto getItemRequestsById(
-            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId,
-            @PathVariable Long itemRequestId
+    @GetMapping("/{requestId}")
+    public ItemRequestDto getItemRequestById(
+            @PathVariable Long requestId,
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId
     ) {
-        return itemRequestService.getItemRequestById(itemRequestId, userId);
+        ItemRequestControllerLoggerHelper.getItemRequestDtoById(log, requestId, userId);
+        return itemRequestService.getItemRequestById(requestId, userId);
     }
 
     @GetMapping("/all")
-    public List<ItemRequestDto> getPageWithItemRequestsByRequesterId(
-            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long userId,
+    public List<ItemRequestDto> getItemRequestPageByRequesterId(
+            @RequestHeader(HttpHeadersConstants.X_SHARER_USER_ID) Long requesterUserId,
             @RequestParam Integer from,
             @RequestParam Integer size
     ) {
-        return itemRequestService.getItemRequestsByRequesterId(userId, from, size);
+        ItemRequestControllerLoggerHelper.getItemRequestDtoPageByRequesterId(log, requesterUserId, from, size);
+        return itemRequestService.getItemRequestsByRequesterId(requesterUserId, from, size);
     }
 }
