768723bca805613b66e341dcab2affea10ea91c2
IvanMarakanov <marakanovivan@gmail.com>
2023-10-07 11:35:24 +0300

    delete: traces of manual mapping

--- server/src/main/java/ru/practicum/shareit/request/dto/response/ItemRequestDto.java
+++ server/src/main/java/ru/practicum/shareit/request/dto/response/ItemRequestDto.java
@@ -3,11 +3,8 @@ package ru.practicum.shareit.request.dto.response;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import ru.practicum.shareit.item.model.Item;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @Setter
 @Getter
@@ -21,35 +18,11 @@ public class ItemRequestDto {
     @Setter
     @Getter
     @NoArgsConstructor
-    @SuppressWarnings("java:S2972")
     public static class ItemDto {
         private Long id;
         private String name;
         private String description;
         private Boolean available;
         private Long requestId;
-
-        public static ItemDto fromItem(Item item) {
-            var itemDto = new ItemDto();
-
-            itemDto.setId(itemDto.getId());
-            itemDto.setName(item.getName());
-            itemDto.setDescription(item.getDescription());
-            itemDto.setAvailable(item.getIsAvailable());
-            itemDto.setRequestId(item.getItemRequest().getId());
-
-            return itemDto;
-        }
-
-        public static List<ItemDto> fromItem(Iterable<Item> items) {
-            var itemDtos = new ArrayList<ItemDto>();
-
-            for (Item item : items) {
-                var itemDto = fromItem(item);
-                itemDtos.add(itemDto);
-            }
-
-            return itemDtos;
-        }
     }
 }
