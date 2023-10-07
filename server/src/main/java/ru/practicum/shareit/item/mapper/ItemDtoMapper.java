0189f3a3e7e7429f2d41fe6e5cd3d8544628dd8a
IvanMarakanov <marakanovivan@gmail.com>
2023-10-07 10:28:36 +0300

    style: location of methods

--- server/src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
+++ server/src/main/java/ru/practicum/shareit/item/mapper/ItemDtoMapper.java
@@ -23,11 +23,11 @@ public interface ItemDtoMapper {
     @Mapping(target = "available", source = "isAvailable")
     @Mapping(target = "requestId", source = "item.itemRequest.id")
     SimpleItemResponseDto mapToSimpleItemResponseDto(Item item);
-
     List<SimpleItemResponseDto> mapToSimpleItemResponseDto(Iterable<Item> items);
 
     @Mapping(target = "authorName", source = "comment.author.name")
     ItemDetailsResponseDto.CommentDto mapToCommentDto(Comment comment);
+    List<ItemDetailsResponseDto.CommentDto> mapToCommentDto(Iterable<Comment> comment);
 
     @Mapping(target = "bookerId", source = "booking.booker.id")
     ItemDetailsResponseDto.BookingDto mapToBookingDto(Booking booking);
@@ -37,20 +37,17 @@ public interface ItemDtoMapper {
     @Mapping(target = "comments", ignore = true)
     @Mapping(target = "available", source = "item.isAvailable")
     ItemDetailsResponseDto mapToItemDetailsResponseDto(Item item);
-
-    @Mapping(target = "lastBooking", ignore = true)
-    @Mapping(target = "nextBooking", ignore = true)
-    @Mapping(target = "comments", ignore = true)
     List<ItemDetailsResponseDto> mapToItemDetailsResponseDto(Iterable<Item> items);
 
     @Mapping(target = "lastBooking", ignore = true)
     @Mapping(target = "nextBooking", ignore = true)
     @Mapping(target = "available", source = "item.isAvailable")
-    ItemDetailsResponseDto mapToItemDetailsResponseDto(Item item, List<Comment> comments);
+    ItemDetailsResponseDto mapToItemDetailsResponseDto(Item item, Iterable<Comment> comments);
 
     @Mapping(target = "id", source = "item.id")
     @Mapping(target = "available", source = "item.isAvailable")
     @Mapping(target = "lastBooking", source = "last")
     @Mapping(target = "nextBooking", source = "next")
-    ItemDetailsResponseDto mapToItemDetailsResponseDto(Item item, List<Comment> comments, Booking last, Booking next);
+    ItemDetailsResponseDto mapToItemDetailsResponseDto(Item item, Iterable<Comment> comments,
+                                                       Booking last, Booking next);
 }
