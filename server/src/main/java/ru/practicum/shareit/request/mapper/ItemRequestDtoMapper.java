85adf6380c662756f866a08af81afccb34ed96b7
IvanMarakanov <marakanovivan@gmail.com>
2023-10-07 11:34:00 +0300

    update: use MapStruct for mapping ItemRequest

--- server/src/main/java/ru/practicum/shareit/request/mapper/ItemRequestDtoMapper.java
+++ server/src/main/java/ru/practicum/shareit/request/mapper/ItemRequestDtoMapper.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.request.mapper;
 
-import lombok.experimental.UtilityClass;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.request.dto.request.RequestItemRequestDto;
 import ru.practicum.shareit.request.dto.response.ItemRequestDto;
@@ -8,65 +9,25 @@ import ru.practicum.shareit.request.model.ItemRequest;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
-import java.util.Collection;
+import java.util.List;
 
-/**
- * A utility class for mapping {@link ItemRequest} objects and their DTOs.
- */
+@Mapper(componentModel = "spring")
+public interface ItemRequestDtoMapper {
 
-@UtilityClass
-public final class ItemRequestDtoMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "creationTime", source = "time")
+    ItemRequest mapToItemRequest(RequestItemRequestDto requestDto, User requester, LocalDateTime time);
 
-    /**
-     * Converts a {@link RequestItemRequestDto} object to an {@link  ItemRequest} object.
-     *
-     * @param itemRequestDto the {@link RequestItemRequestDto} to convert.
-     * @param requester      the {@link User} who made the request.
-     * @param time           the LocalDateTime when the request was created.
-     * @return the converted {@link ItemRequest} object.
-     */
-    public static ItemRequest toItemRequest(RequestItemRequestDto itemRequestDto,
-                                            User requester, LocalDateTime time) {
-        ItemRequest itemRequest = new ItemRequest();
+    @Mapping(target = "items", ignore = true)
+    @Mapping(target = "created", source = "creationTime")
+    ItemRequestDto mapToItemRequestDto(ItemRequest request);
 
-        itemRequest.setDescription(itemRequestDto.getDescription());
-        itemRequest.setRequester(requester);
-        itemRequest.setCreationTime(time);
+    @Mapping(target = "created", source = "request.creationTime")
+    ItemRequestDto mapToItemRequestDto(ItemRequest request, Iterable<Item> items);
 
-        return itemRequest;
-    }
+    @Mapping(target = "available", source = "item.isAvailable")
+    @Mapping(target = "requestId", source = "item.itemRequest.id")
+    ItemRequestDto.ItemDto mapToItemRequestItemDto(Item item);
+    List<ItemRequestDto.ItemDto> mapToItemRequestItemDto(Iterable<Item> item);
 
-    /**
-     * Converts an {@link ItemRequest} object to an
-     * {@link ItemRequestDto} object without its items.
-     *
-     * @param itemRequest the {@link ItemRequest} to convert.
-     * @return the converted {@link ItemRequestDto} object.
-     */
-    public static ItemRequestDto toItemRequestDtoWithoutItems(ItemRequest itemRequest) {
-        ItemRequestDto itemRequestDto = new ItemRequestDto();
-
-        itemRequestDto.setId(itemRequest.getId());
-        itemRequestDto.setDescription(itemRequest.getDescription());
-        itemRequestDto.setCreated(itemRequest.getCreationTime());
-
-        return itemRequestDto;
-    }
-
-    /**
-     * Converts an {@link ItemRequest} object and a collection
-     * of its items to an {@link ItemRequestDto} object.
-     *
-     * @param itemRequest the {@link ItemRequest} to convert.
-     * @param items       the collection of {@link Item} objects
-     *                    associated with the request.
-     * @return the converted {@link ItemRequestDto} object.
-     */
-    public static ItemRequestDto toItemRequestDto(ItemRequest itemRequest, Collection<Item> items) {
-        ItemRequestDto itemRequestDto = toItemRequestDtoWithoutItems(itemRequest);
-
-        itemRequestDto.setItems(ItemRequestDto.ItemDto.fromItem(items));
-
-        return itemRequestDto;
-    }
 }
