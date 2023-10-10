1c38aa957ff037007ca608fa1fc80224b946194f
IvanMarakanov <marakanovivan@gmail.com>
2023-10-10 12:52:04 +0300

    refactor: methods in CRUD order

--- server/src/main/java/ru/practicum/shareit/user/logger/UserControllerLoggerHelper.java
+++ server/src/main/java/ru/practicum/shareit/user/logger/UserControllerLoggerHelper.java
@@ -11,16 +11,16 @@ public class UserControllerLoggerHelper {
         log.info("Adding user: {}", userDto);
     }
 
-    public void updateUser(Logger log, UserCreationDto userDto, Long userId) {
-        log.info("Updating user with id: {}, {}", userId, userDto);
-    }
-
     public void getUserDtoById(Logger log, Long userId) {
         log.info("Getting user by id: {}", userId);
     }
 
     public void getUserDtoPage(Logger log, Integer from, Integer size) {
         log.info("Getting users with pagination: (from: {}, size: {})", from, size);
+    }
+
+    public void updateUser(Logger log, UserCreationDto userDto, Long userId) {
+        log.info("Updating user with id: {}, {}", userId, userDto);
     }
 
     public void deleteUserById(Logger log, Long userId) {
