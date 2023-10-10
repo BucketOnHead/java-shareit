d5ab6d6faf06c65173f532bcf2078538b48380a7
IvanMarakanov <marakanovivan@gmail.com>
2023-10-10 11:25:06 +0300

    add: serialVersionUID to UserNotFoundException

--- server/src/main/java/ru/practicum/shareit/user/exception/UserNotFoundException.java
+++ server/src/main/java/ru/practicum/shareit/user/exception/UserNotFoundException.java
@@ -4,6 +4,8 @@ import lombok.NonNull;
 import ru.practicum.shareit.exception.EntityNotFoundException;
 
 public class UserNotFoundException extends EntityNotFoundException {
+    private static final long serialVersionUID = -2546797154273459251L;
+
     public UserNotFoundException(String message) {
         super(message);
     }
