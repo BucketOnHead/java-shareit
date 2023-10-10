3e1cd7907108a3e1c370abd36aedad86eb269b08
IvanMarakanov <marakanovivan@gmail.com>
2023-10-10 11:46:58 +0300

    add: `@ToString` to UserCreationDto

--- server/src/main/java/ru/practicum/shareit/user/dto/request/UserCreationDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/request/UserCreationDto.java
@@ -3,9 +3,11 @@ package ru.practicum.shareit.user.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
-@Setter
 @Getter
+@Setter
+@ToString
 @NoArgsConstructor
 public class UserCreationDto {
     private String name;
