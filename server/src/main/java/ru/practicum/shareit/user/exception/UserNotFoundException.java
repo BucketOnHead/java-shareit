e906ab2c07324e7b836b11f33f9417150db6a7fd
IvanMarakanov <marakanovivan@gmail.com>
2023-10-10 11:07:26 +0300

    refactor: decrease abstraction UserNotFoundException

--- server/src/main/java/ru/practicum/shareit/user/exception/UserNotFoundException.java
+++ server/src/main/java/ru/practicum/shareit/user/exception/UserNotFoundException.java
@@ -1,16 +1,15 @@
 package ru.practicum.shareit.user.exception;
 
+import lombok.NonNull;
 import ru.practicum.shareit.exception.EntityNotFoundException;
 
 public class UserNotFoundException extends EntityNotFoundException {
-    private static final String USER_NOT_FOUND = "USER[ID_%d] not found";
-
     public UserNotFoundException(String message) {
         super(message);
     }
 
-    public static UserNotFoundException getFromUserId(Long userId) {
-        String message = String.format(USER_NOT_FOUND, userId);
+    public static UserNotFoundException byId(@NonNull String userId) {
+        String message = String.format("User not fount with id: %s", userId);
         return new UserNotFoundException(message);
     }
 }
--- server/src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ server/src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -16,7 +16,7 @@ public interface UserRepository extends JpaRepository<User, Long> {
      */
     default void validateUserExistsById(Long userId) {
         if (!existsById(userId)) {
-            throw UserNotFoundException.getFromUserId(userId);
+            throw UserNotFoundException.byId(userId.toString());
         }
     }
 }
