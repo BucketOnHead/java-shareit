4f0d79bbb8264638a7ffa0a1c10317adfa45da43
IvanMarakanov <marakanovivan@gmail.com>
2023-10-13 12:08:46 +0300

    fix: bug

--- server/src/main/java/ru/practicum/shareit/booking/utils/BookingUtils.java
+++ server/src/main/java/ru/practicum/shareit/booking/utils/BookingUtils.java
@@ -7,19 +7,15 @@ import ru.practicum.shareit.booking.model.BookingStatus;
 @UtilityClass
 public class BookingUtils {
 
-    public boolean isWaiting(Booking booking) {
+    public boolean isNotWaiting(Booking booking) {
         if (booking == null) {
             return false;
         }
 
-        return booking.getStatus() == BookingStatus.WAITING;
+        return booking.getStatus() != BookingStatus.WAITING;
     }
 
-    public boolean isNotWaiting(Booking booking) {
-        return !isWaiting(booking);
-    }
-
-    public boolean isBooker(Booking booking, Long userId) {
+    public boolean isNotBooker(Booking booking, Long userId) {
         if (booking == null || userId == null) {
             return false;
         }
@@ -34,10 +30,6 @@ public class BookingUtils {
             return false;
         }
 
-        return bookerId.equals(userId);
-    }
-
-    public boolean isNotBooker(Booking booking, Long userId) {
-        return !isBooker(booking, userId);
+        return !bookerId.equals(userId);
     }
 }
